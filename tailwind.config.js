d53e11ed950e6f0351c6c4f499ea5670a30b69b8
Mwikala Kangwa <mwikala@yawt.digital>
2023-03-25 11:26:04 +0000

    fix: broken tailwind prefix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: "ywt-",
-  content: [
-    "./storage/framework/views/*.php",
-    "./resources/**/*.blade.php",
-    "./resources/**/*.ts",
-    "./resources/**/*.vue",
-  ],
+  content: ["./storage/framework/views/*.php", "./resources/**/*.blade.php", "./resources/**/*.{ts,js,vue}"],
   theme: {
     extend: {
       fontFamily: {
